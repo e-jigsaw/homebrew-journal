2fe21c0c8a4126503abc037d145c6893765b75a4
jigsaw <jigsaw@live.jp>
2014-06-29 03:56:00 +0900

    fix sha1

--- journal.rb
+++ journal.rb
@@ -6,8 +6,10 @@ class Journal < Formula
 
   if Hardware.is_64_bit?
     url "https://github.com/e-jigsaw/journal/releases/download/v0.1.0/journal_darwin_amd64"
+    sha1 "922522ecb42245a241607e7a063647e49c83a054"
   else
     url "https://github.com/e-jigsaw/journal/releases/download/v0.1.0/journal_darwin_386"
+    sha1 "3ab4eb0f2815e017c21d7aa38f8ad1dcd0a2f82c"
   end
 
   depends_on :arch => :intel
