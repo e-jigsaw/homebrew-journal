46b79cf9e165a3fe78dbd7d40829f95aacc04bef
jigsaw <jigsaw@live.jp>
2014-06-30 10:28:54 +0900

    update 0.1.1

--- journal.rb
+++ journal.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Journal < Formula
   homepage "https://github.com/e-jigsaw/journal"
-  version '0.1.0'
+  version '0.1.1'
 
   if Hardware.is_64_bit?
-    url "https://github.com/e-jigsaw/journal/releases/download/v0.1.0/journal_darwin_amd64.zip"
-    sha1 "9d064afed582d938920157b9f56020511811c066"
+    url "https://github.com/e-jigsaw/journal/releases/download/v0.1.1/journal_darwin_amd64.zip"
+    sha1 "178b960af015aaeaf95e12e9a964f387420956e6"
   else
-    url "https://github.com/e-jigsaw/journal/releases/download/v0.1.0/journal_darwin_386.zip"
-    sha1 "28e392b144ef4d4c890d4bf209083af907c19cc1"
+    url "https://github.com/e-jigsaw/journal/releases/download/v0.1.1/journal_darwin_386.zip"
+    sha1 "39fb512895bff623eb0fcb5c85edf88874c98a57"
   end
 
   depends_on :arch => :intel
